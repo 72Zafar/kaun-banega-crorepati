b4ec4aca918666e7b2611905ea0a6710959e2b74
Zafar Ali Khan <168206865+72Zafar@users.noreply.github.com>
2024-06-15 12:54:45 -0700

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,78 @@
+print ("         # Kaun Banega #       ")
+
+Qustens =[
+["How many tigers in south africa"]
+
+]
+answers = [
+    ["Her is cagens"],
+
+    ["a"],
+    [359],
+    ["b"],
+    [195],
+    ["c"],
+    ["200 billion trillon"],
+    ["d"],
+    [104]
+]
+
+for i in Qustens:
+    print (i)
+
+for a in answers:
+    print (a)
+
+print ("Her is price")
+price = [
+    [1],
+    [2000],
+    [2],
+    [3000],
+    [3],
+    [5000],
+    [4],
+    [10000]
+]
+print (price)
+def cheek():
+     answers = input("Enter your questen \n")
+     for a in answers:
+      print (a)
+     if answers == "a":
+         print("Good and you win 2000")
+     else:
+         print ("Ronge")
+         print ("Game Over")    
+
+cheek()        
+
+q = ["How many countries in the world"]
+print (q)
+
+answers = [
+    ["Her is cagens"],
+
+    ["a"],
+    [359],
+    ["b"],
+    [195],
+    ["c"],
+    ["200 billion trillon"],
+    ["d"],
+    [104]
+]
+
+for a in answers:
+    print (a)
+
+def cheek():
+     answers = input("Enter your questen \n")
+     print (answers)
+     if answers == "b":
+         print("Good and you win 3000")
+     else:
+         print ("Ronge")
+         print ("Game Over")    
+cheek()
+
